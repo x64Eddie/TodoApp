070c2985558f650bdc60e4d9c29f569e0a87cc92
Eddie <eherrerag.eh@gmail.com>
2019-02-02 17:55:10 -0700

    Now the items are being displayed in the screen, its being grabed from the data base.

--- lib/UI/notodo_screen.dart
+++ lib/UI/notodo_screen.dart
@@ -11,13 +11,25 @@ class NoTodoScreen extends StatefulWidget{
 class _NoTodoScreen extends State<NoTodoScreen>{
   final TextEditingController _textEditingController = new TextEditingController();
   var db = new DatabaseHelper();
+  final List<NoDoItem> _itemList = <NoDoItem>[];
 
+  @override
+  void initState() {
+    super.initState();
+    _readNoDoList();
+  }
   //Handling the message that the user inserted into the app
   void _handleSubmitted(String text) async{
     _textEditingController.clear();
 
     NoDoItem noDoItem = new NoDoItem(text, DateTime.now().toIso8601String());
     int savedItemId = await db.saveItem(noDoItem);
+
+    NoDoItem addedItem = await db.getItem(savedItemId);
+
+    setState(() {
+      _itemList.insert(0, addedItem);
+    });
 
     print("Item Saved id: $savedItemId");
   }
@@ -26,7 +38,33 @@ class _NoTodoScreen extends State<NoTodoScreen>{
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Colors.black87,
-      body: Column(),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+              child: new ListView.builder(
+                  padding: new EdgeInsets.all(8.0),
+                  reverse: false,
+                  itemCount: _itemList.length,
+                  itemBuilder: (_, int index){
+                    return new Card(
+                      color: Colors.white10,
+                      child: new ListTile(
+                        title: _itemList[index],
+                        onLongPress: () => debugPrint(""),
+                        trailing: new Listener(
+                          key: new Key(_itemList[index].itemName),
+                          child: new Icon(Icons.remove_circle),
+                          onPointerDown: (pointerEvent) => debugPrint(""),
+                        ),
+                      ),
+                    );
+                  })
+          ),
+          new Divider(
+            height: 1.0,
+          )
+        ],
+      ),
       floatingActionButton: new FloatingActionButton(
           tooltip: "Add Item",
           backgroundColor: Colors.redAccent,
@@ -74,5 +112,13 @@ class _NoTodoScreen extends State<NoTodoScreen>{
         builder:(_) {
           return alert;
         });
+  }
+  _readNoDoList() async{
+    List items = await db.getItems();
+    items.forEach((item){
+      NoDoItem noDoItem = NoDoItem.map(item);
+
+      print("Db Items: ${noDoItem.itemName}");
+    });
   }
 }
